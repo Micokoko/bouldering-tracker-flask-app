42635c2b67831c0480e1586c87a759a5e876b4fb
micokoko <mico.balina@gmail.com>
2024-09-06 19:03:24 +0800

    log_ascent route inc

--- bouldering_app/log_ascent.py
+++ bouldering_app/log_ascent.py
@@ -6,5 +6,8 @@ from .auth import login_required
 
 bp = Blueprint('log_ascent', __name__, url_prefix='/climber')
 
-def log_ascent_user():
+
+@bp.route('/<int:boulder_id>/log_ascent', methods=('GET', 'POST'))
+@login_required
+def log_ascent_user(boulder_id):
     pass
